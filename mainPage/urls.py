c19c9c535716bd02bcec7b30a77e5844a6dbdb53
timof <timofeybobrovskiy@outlook.com>
2023-06-03 20:24:23 +0300

    Project created

---
+++ mainPage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('/', views.getHomePage, name='getHomePage'),
+    path('', views.getHomePage, name='getHomePage'),
+    path('about/', views.getAboutPage),
+    path('contacts/', views.getContactsPage)
+]
---
+++ mainPage/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, HttpResponse
+from django.views.generic import TemplateView
+
+
+
+# Create your views here.
+import requests
+
+
+
+
+def getHomePage(request):
+
+    
+    if request.META.get('HTTP_HX_REQUEST'):
+        header_template = 'empty_template.html' 
+        footer_template = 'empty_template.html' 
+        head_template = 'head.html'
+    else:
+        header_template = 'header.html'
+        footer_template = 'footer.html'
+        head_template = 'head.html'
+    
+    return render(
+        request,
+        "home.html",
+        {'header_template': header_template,
+        'footer_template': footer_template,
+        'head_template': head_template}
+        )
+
+
+
+def getAboutPage(request):
+    if request.META.get('HTTP_HX_REQUEST'):
+        header_template = 'empty_template.html' 
+        footer_template = 'empty_template.html' 
+        head_template = 'head.html'
+    else:
+        header_template = 'header.html'
+        footer_template = 'footer.html'
+        head_template = 'head.html'
+    
+    return render(
+        request,
+        "about.html",
+        {'header_template': header_template,
+        'footer_template': footer_template,
+        'head_template': head_template}
+        )
+
+
+
+def getContactsPage(request):
+    if request.META.get('HTTP_HX_REQUEST'):
+        header_template = 'empty_template.html' 
+        footer_template = 'empty_template.html' 
+        head_template = 'head.html'
+    else:
+        header_template = 'header.html'
+        footer_template = 'footer.html'
+        head_template = 'head.html'
+    
+    return render(
+        request,
+        "contactsPage.html",
+        {'header_template': header_template,
+        'footer_template': footer_template,
+        'head_template': head_template}
+        )
